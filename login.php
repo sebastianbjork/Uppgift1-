f35e33ee99393f9b00e762939b104cdf78684df6
sebastianbjork <sebastian.bjorkskog@optistud.fi>
2016-01-12 10:18:25 +0200

    index added

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Login Page</title>
+  <link rel="stylesheet" href="css/foundation.css" />
+  <link rel="stylesheet" href="css/app.css" />
+</head>
+<body>
+
+  <div class="large-3 large-centered columns">
+    <div class="login-box">
+    <div class="row">
+    <div class="large-12 columns">
+      <form>
+         <div class="row">
+           <div class="large-12 columns">
+               <input type="text" name="username" placeholder="Username" />
+           </div>
+         </div>
+        <div class="row">
+           <div class="large-12 columns">
+               <input type="password" name="password" placeholder="Password" />
+           </div>
+        </div>
+        <div class="row">
+          <div class="large-12 large-centered columns">
+            <input type="submit" class="button expand" value="Log In"/>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+  </div>
+  </div>
+
+</body>
+</html>
